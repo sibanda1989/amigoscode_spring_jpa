5ab6d834b555f27ae9edef4a328ee9b77f9907f6
Hastings Sibanda <hastings.sibanda@liquid.tech>
2023-08-01 09:37:26 +0200

    feat: implement delete func

--- src/main/java/com/stingtech/demo/student/StudentService.java
+++ src/main/java/com/stingtech/demo/student/StudentService.java
@@ -31,4 +31,16 @@ public class StudentService {
         studentRepository.save(student);
         System.out.println(student);
     }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        //first check if student exists
+        if (!exists){
+            throw new IllegalStateException(
+                    "student with id " + studentId + " does not exist"
+            );
+        }
+
+        studentRepository.deleteById(studentId);
+    }
 }
